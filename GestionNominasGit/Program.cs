7d9aa067185438310535a6fc1d0ae2555ea1e86a
DAM 1 <DAM 1@DESKTOP-B02MP2D>
2024-01-16 08:53:33 +0100

    Agregar archivos de proyecto.

---
+++ GestionNominasGit/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace GestionNominasGit
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Controlador Principal
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            // CONSTANTESS
+
+            // RECURSOS
+
+            // INICIALIZACIÓN
+
+            // ENTRADA
+
+            // PROCESO
+
+            // SALIDA
+        }
+    }
+}
