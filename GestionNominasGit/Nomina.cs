cc14a74ea4fe7d89696ae39fd4424f20a201896d
Diego Torres Contreras <dietoco92@hotmail.com>
2024-01-16 09:32:41 +0100

    Creación de la Propiedad NombreEmpleado

--- GestionNominasGit/Nomina.cs
+++ GestionNominasGit/Nomina.cs
@@ -14,5 +14,35 @@ namespace GestionNominasGit
         private string _puesto;
         private int _horasTrabajadas;
         private float _salarioHora;
+
+        // PROPIEDADES
+        /// <summary>
+        /// Nombre del Empleado
+        /// </summary>
+        public string NombreEmpleado
+        {
+            get 
+            {
+                // Control de Contenido
+                if (string.IsNullOrEmpty(_nombre))
+                {
+                    throw new Exception("Nombre del trabajador no establecido.");
+                }
+
+                // Devolución del Valor
+                return _nombre;
+            }
+            set 
+            {
+                // Validación de los Datos a Establecer
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception("El nombre del trabajador no puede ser Null o la cadena vacía.");
+                }
+
+                // Establecimiento del valor al miembro privado
+                _nombre = value;
+            }
+        }
     }
 }
